0ab777cbdbf9340a59407d0e27345e5517062642
abiodun_solomon <habbeboynetwork@gmail.com>
2018-07-23 19:28:28 +0100

    updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,7 +80,12 @@
         </main>
     </div>
 
+
     <!-- Scripts -->
+    <script
+  src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/portal.js')}}" ></script>
 
